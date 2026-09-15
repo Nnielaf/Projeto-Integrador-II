b6690bd3a865d10ea87d0825e1294d3f66831c19
Nnielaf <daangeloo2023@gmail.com>
2026-09-15 20:46:43 -0300

    Create Exer3.ts

---
+++ Projeto Integrador- 2/Projeto Integrador/Exercicios/Exer3.ts
@@ -0,0 +1,9 @@
+let ganhos = Number (prompt("Infome a quantia de ganhos :"));
+let gastos = Number (prompt("Infome a quantia de gastos :"));
+
+if(ganhos > gastos){
+    console.log("Você está dentro do orçamento!")
+}
+else{
+    console.log("Você está fora do orçamento! Não gaste mais!")
+}
