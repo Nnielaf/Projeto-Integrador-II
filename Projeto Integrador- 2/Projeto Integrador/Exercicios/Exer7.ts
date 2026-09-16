ba862c456a869aa7372dcdc5391b1667f46ce9dc
Nnielaf <daangeloo2023@gmail.com>
2026-09-16 16:34:30 -0300

    Update Exer7.ts

--- Projeto Integrador- 2/Projeto Integrador/Exercicios/Exer7.ts
+++ Projeto Integrador- 2/Projeto Integrador/Exercicios/Exer7.ts
@@ -1,23 +1,23 @@
 function medirTemperatura(temp : number):void{
     let situacao = "";
     if(temp <= 36){
-        situacao = "Hipotermia"
+        situacao = "A sua temperatura é Hipotermia"
         console.log(situacao)
     }
     else if( temp >= 36 && temp <= 37.6){
-        situacao="Normal"
+        situacao="A sua temperatura é Normal"
         console.log(situacao)
     }
     else if(temp >=37.6 && temp <= 39.6){
-        situacao = "Febre"
+        situacao = "A sua temperatura é Febre"
         console.log(situacao)
     }
     else if(temp >= 39.6 && temp <= 41){
-        situacao = "Febre Alta"
+        situacao = "A sua temperatura é Febre Alta"
         console.log(situacao)
     }
     else{
-        situacao="Hipertemia"
+        situacao="A sua temperatura é Hipertemia"
         console.log(situacao)
     }
 }
