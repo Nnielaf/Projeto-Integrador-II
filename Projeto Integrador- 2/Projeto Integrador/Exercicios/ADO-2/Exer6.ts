97eef40aa2bd4c7c07d06b9748a802c03b5d1674
Nnielaf <daangeloo2023@gmail.com>
2026-09-17 17:43:14 -0300

    Create Exer6.ts

---
+++ Projeto Integrador- 2/Projeto Integrador/Exercicios/ADO-2/Exer6.ts
@@ -0,0 +1,11 @@
+let comeco= Number(prompt("Digite um numero"))
+
+let bolinha = "*"
+let contador = 0;
+let resultado = "";
+
+while(contador <= comeco){
+    resultado = resultado + bolinha;
+    contador++;
+}
+console.log(resultado)
