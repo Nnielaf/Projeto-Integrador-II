ad6eedf06c8fb3d0474a3988aaadb5aca97d5ec9
Nnielaf <daangeloo2023@gmail.com>
2026-09-17 17:19:44 -0300

    Create Exer4.ts

---
+++ Projeto Integrador- 2/Projeto Integrador/Exercicios/ADO-2/Exer4.ts
@@ -0,0 +1,8 @@
+let comeco= Number(prompt("Digite um numero"))
+let fim = Number(prompt("Digite o numero final"));
+let contador = comeco
+
+while(contador >= fim){
+    console.log(contador)
+    contador--;
+}
